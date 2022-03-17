cd291269c084332c3a594b895f3ccc88e7d38a66
Morgany Vieira Mendes <morgany@mangustotech.com>
2022-03-17 17:08:04 -0300

    Update create_instance.py: fix to show master instances

--- trove_dashboard/content/databases/workflows/create_instance.py
+++ trove_dashboard/content/databases/workflows/create_instance.py
@@ -440,7 +440,7 @@ class AdvancedAction(workflows.Action):
         try:
             instances = self._get_instances()
             choices = sorted([(i.id, i.name) for i in
-                             instances if i.status == 'ACTIVE'],
+                             instances if i.status == 'HEALTHY'],
                              key=lambda i: i[1])
         except Exception:
             choices = []
